53a46698946e2473334562dae8ab233f1289f53b
SageMystic <147736783+SageMystic@users.noreply.github.com>
2024-01-25 23:00:19 +0300

    добавление комметариев

--- constants.py
+++ constants.py
@@ -1,12 +1,12 @@
-WIDTH = 1300
-HEIGHT = 700
+WIDTH = 1300 #ширина
+HEIGHT = 800 #высота (гриша не бей (￣m￣）)
 BLACK = (10, 10, 10)
 WHITE = (255, 255, 255)
 GRAY = (200, 200, 200)
 GREEN = (80, 255, 100)
-RED = (255, 120, 0)
-FONT_SIZE = 30
+RED = (255, 120, 10)
+FONT_SIZE = 80
 
-PADDLE_OFFSET = 15
-PADDLE_SPEED = 2
-BALL_SPEED = 5
+RACKET_OFFSET = 15 #удалённость ракеток от стенок
+RACKET_SPEED = 5 #скорость ракеток
+BALL_SPEED = 5 ##скорость мяча
