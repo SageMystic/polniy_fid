1fbdda6caff573fca8a46f79001a18457792082e
fahrenghate <128506486+fahrenghate@users.noreply.github.com>
2024-02-13 18:10:25 +0300

    Update dlyakosti.py

--- dlyakosti.py
+++ dlyakosti.py
@@ -7,16 +7,10 @@ ls = []
 with open('houses.csv', 'r') as file:
     r = csv.DictReader(file, delimiter=';')
     for row in r:
-        f = False
-        for i in row:
-            if int(i['inside']) != int(i['outside']):
-                x = abs(int(i['inside']) - int(i['outside']))
-                f = True
-                break
-        if f is True:
-            ls.append({"adress": row['adress'], "difference": x})
+        if int(row['inside']) != int(row['outside']):
+            x = abs(int(row['inside']) - int(row['outside']))
+            ls.append({"address": row['address'], "difference": x})
 
 with open('unusual.json', 'w') as f:
     json.dump(ls, f)
     
-https://lyceum.s3.yandex.net/content/sr/23extra2lite3/houses.csv
